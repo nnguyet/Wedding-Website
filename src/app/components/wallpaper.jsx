764e6a68f7524430fa65465f10b53eae377b0994
nnguyet <nguyetlms@gmail.com>
2023-12-09 19:25:25 +0700

    delete wallpaper fix on small desktop

--- src/app/components/wallpaper.jsx
+++ src/app/components/wallpaper.jsx
@@ -5,27 +5,27 @@ export const Wallpaper = () => {
         <Flex
             justifyContent={'center'}
             alignItems={'center'}
-            pt={{base:'0', md:'1em'}}
+            pt={{base:'0', md:'0.75em'}}
             px={{base:'0', md:'3em'}}
             bg={'pink.light'}
         >
             <Image
                 src='/images/wallpaper01.jpg'
                 alt='wallpaper1'
-                w={{base:'33.33%', md:'29%'}}
-                px={{base:'0', md:'0.5em'}}
+                w={{base:'33.33%', md:'28%'}}
+                px={{base:'0', md:'0.375em'}}
             />
             <Image
                 src='/images/wallpaper02.jpg'
                 alt='wallpaper2'
-                w={{base:'33.33%', md:'29%'}}
-                px={{base:'0', md:'0.5em'}}
+                w={{base:'33.33%', md:'28%'}}
+                px={{base:'0', md:'0.375em'}}
             />
             <Image
                 src='/images/wallpaper03.jpg'
                 alt='wallpaper3'
-                w={{base:'33.33%', md:'29%'}}
-                px={{base:'0', md:'0.5em'}}
+                w={{base:'33.33%', md:'28%'}}
+                px={{base:'0', md:'0.375em'}}
             />
         </Flex>
     )
