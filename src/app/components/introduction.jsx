82a5c5ac73e609cc7d7af158878297e9ab79f107
nnguyet <nguyetlms@gmail.com>
2023-12-22 22:52:47 +0700

    fix error Text in Text in introduction section

--- src/app/components/introduction.jsx
+++ src/app/components/introduction.jsx
@@ -25,12 +25,17 @@ export const Introduction = () => {
                  rong ruổi khắp VT-SG và chuẩn bị chiếc hôn lễ này.
                  Không ít sự vất vả, không ít sự bất đồng quan điểm, cũng không ít sự cãi vã,
                  nhưng chúng mình vui khi được thực hiện hóa ước mơ cùng nhau.
-                <Text
-                    p={{base:'0.5em 0 0', md:'0.75em 0 0'}}
-                >
-                    Đó sẽ một ngày gió nhẹ, có nắng vàng, và chúng mình bên nhau,
-                     bên những người thương nhất, được họ chứng kiến và cứ thế trở thành vợ chồng.
-                </Text>
+            </Text>
+            <Text
+                p={{base:'0.5em 0 0', md:'0.75em 0 0'}}
+                textAlign={{base:'center'}}
+                lineHeight={{ base: "1.5em", md:'1.5em' }}
+                fontSize={{ base: "1em", md:'1.25em' }}
+                w={{md:'80%', lg:'60%'}}
+                m={{base:'auto'}}
+            >
+                Đó sẽ một ngày gió nhẹ, có nắng vàng, và chúng mình bên nhau,
+                    bên những người thương nhất, được họ chứng kiến và cứ thế trở thành vợ chồng.
             </Text>
         </Box>
     )
