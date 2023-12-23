47e02e2901b95b1b7ca4eeb38306cefa14216094
nnguyet <nguyetlms@gmail.com>
2023-12-24 03:34:54 +0700

    add qr code to wishes page

--- src/app/wishes/page.jsx
+++ src/app/wishes/page.jsx
@@ -1,14 +1,16 @@
 import { Image, Box, Text, Heading, Tabs, Tab, TabList, TabPanels, TabPanel, Center } from '@chakra-ui/react'
 import { Form, Messages } from '../components/wishes.section';
+import { Qrcode } from '../components';
 
 export default function Wishes() {
     return (
         <Box
-            p={{base:'2em 0 1em'}}
+            p={{base:'2em 0 0'}}
             bg={'pink.light'}
         >
             <Form />
             <Messages />
+            <Qrcode />
         </Box>
     );
 }
