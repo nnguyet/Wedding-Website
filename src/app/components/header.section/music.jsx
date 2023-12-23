4c0f48fdc8d0f214b2e4c81fdc93fda0312c517a
nnguyet <nguyetlms@gmail.com>
2023-12-23 18:51:59 +0700

    fix play music button UI (mobile)

--- src/app/components/header.section/music.jsx
+++ src/app/components/header.section/music.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useRef } from 'react';
-import { Button } from '@chakra-ui/react'
+import { Button, Box, Flex } from '@chakra-ui/react'
 // import for icon
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
@@ -17,6 +17,7 @@ export const Music = () => {
 
     const handleMusic = (e) => {
         e.preventDefault();
+        console.log(pause.current);
         if (musicAudio.current.paused) {
             play.current.classList.add('hidden');
             pause.current.classList.remove('hidden');
@@ -31,21 +32,43 @@ export const Music = () => {
     }
 
     return (
-        <Button
-            m={{base:'0.5em 0.5em 0.5em 0', md:'0 0 0 1em'}}
+        <Flex
+            m={{base:'0.5em 0.5em 0.75em 0', md:'0 0 0 1em'}}
             fontSize={{base:'0.7em', md:'0.8em'}}
             className='hover:bg-transparent hover:text-textColor'
             color={{base:'pink.light'}}
             border={{base:'2px solid white'}}
             borderRadius={{base:'50%'}}
-            size={{base:'sm', sm:'sm'}}
+            // size={{base:'sm', sm:'sm'}}
+            justifyContent={'center'}
+            alignItems={'center'}
+            w={{base:'2.5em'}}
             px={{base:'0.5em', md:'1em'}}
             aspectRatio={{base:'1/1'}}
             onClick={handleMusic}
         >
             <audio ref={musicAudio} src="/music/Beautiful-In-White.mp3" loop />
-            <FontAwesomeIcon className='ml-0.5' ref={play} icon={faPlay}/>
-            <FontAwesomeIcon className='mr-0.5 hidden' ref={pause} icon={faPause}/>
-        </Button>
+            <Flex
+                ref={play}
+                // m={{base:'0.1em 0 0 0.2em'}}
+                w={'100%'}
+                justifyContent={'center'}
+                alignItems={'center'}
+            >
+                <FontAwesomeIcon icon={faPlay}/>
+
+            </Flex>
+            <Flex
+                ref={pause}
+                className='hidden'
+                // m={{base:'0.1em 0 0'}}
+                w={'100%'}
+                justifyContent={'center'}
+                alignItems={'center'}
+            >
+            <FontAwesomeIcon icon={faPause}/>
+
+            </Flex>
+        </Flex>
     )
 }
