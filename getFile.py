41bf82c928882651033164d16c8f5c4211a77233
nnguyet <nguyetlms@gmail.com>
2023-12-24 23:23:53 +0700

    add images to Pre-wedding album

---
+++ getFile.py
@@ -0,0 +1,3 @@
+from os import listdir
+
+print(listdir('./public/albums/pre-wedding'))
--- src/app/components/albumImage.jsx
+++ src/app/components/albumImage.jsx
@@ -1,13 +1,18 @@
 export const albumImage = {
     "pre-wedding":[
-        'img04.jpg', 'img06.jpg',
-        'img07.jpg', 'img08.jpg',
-        'img1.jpg',  'img10.jpg',
-        'img11.jpg', 'img12.jpg',
-        'img13.jpg', 'img14.jpg',
-        'img2.jpg',  'img3.jpg',
-        'img5.jpg',  'img9.jpg'
-
+        'img01.jpg', 'img02.jpg', 'img03.jpg', 'img04.jpg',
+        'img05.jpg', 'img06.jpg', 'img07.jpg', 'img08.jpg',
+        'img09.jpg', 'img10.jpg', 'img11.jpg', 'img12.jpg',
+        'img13.jpg', 'img14.jpg', 'img15.jpg', 'img16.jpg',
+        'img17.jpg', 'img18.jpg', 'img19.jpg', 'img20.jpg',
+        'img21.jpg', 'img22.jpg', 'img23.jpg', 'img24.jpg',
+        'img25.jpg', 'img26.jpg', 'img27.jpg', 'img28.jpg',
+        'img29.jpg', 'img30.jpg', 'img31.jpg', 'img32.jpg',
+        'img33.jpg', 'img34.jpg', 'img35.jpg', 'img36.jpg',
+        'img37.jpg', 'img38.jpg', 'img39.jpg', 'img40.jpg',
+        'img41.jpg', 'img42.jpg', 'img43.jpg', 'img44.jpg',
+        'img45.jpg', 'img46.jpg', 'img47.jpg', 'img48.jpg',
+        'img49.jpg', 'img50.jpg', 'img51.jpg', 'img52.jpg'
     ],
     "le-ruoc-dau":[
     ],
