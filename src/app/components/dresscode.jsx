e386dae5925d3529bd02750df6aa284c5eb6a22b
nnguyet <nguyetlms@gmail.com>
2023-12-25 04:23:05 +0700

    change to smaller size dresscode

--- src/app/components/dresscode.jsx
+++ src/app/components/dresscode.jsx
@@ -12,12 +12,15 @@ export const Dresscode = () => {
                 // px={{base:'1.5em', sm:'2em', md:'5em', lg:'11em'}}
                 mx={'auto'}
                 w={{base:'85%', md:'40em'}}
+                fontSize={{base:'1.0625em', md:'1.125em'}}
             >
                 Xin kính mời các khách quý đến trong khoảng giờ đón khách, để có thể cùng Việt & Ngọc lưu lại các hình ảnh kỷ niệm và dành thời gian trò chuyện trước tiệc cưới nhé!
             </Text>
             <Text
-                mt={{base:'1em'}}
-                mb={{base:'2em'}}
+                mt={{base:'1.5em', lg:'2em'}}
+                mb={{base:'1.5em'}}
+                fontWeight={'600'}
+                fontSize={{base:'1.125em', md:'1.25em'}}
             >
                 Dresscode Tone Pastel
             </Text>
@@ -32,7 +35,7 @@ export const Dresscode = () => {
                         key={index}
                         src={`/dresscode/waxseal${item}.png`}
                         alt='wax-seal'
-                        w={{base:'2.75em', sm:'3em', md:'4em', lg:'4.5em'}}
+                        w={{base:'2.75em', sm:'3em', md:'3.5em', lg:'4em'}}
                         m={{base:'0', sm:'auto 0.5em', md:'auto 1em', lg:'auto 1.5em'}}
                     />
                 ))}
