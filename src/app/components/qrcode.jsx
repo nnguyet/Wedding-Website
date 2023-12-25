27b79fd6c95f6370a51d1e0ac65c09f23fa3f9b4
nnguyet <nguyetlms@gmail.com>
2023-12-25 23:39:33 +0700

    update QR Code section

--- src/app/components/qrcode.jsx
+++ src/app/components/qrcode.jsx
@@ -18,24 +18,61 @@ export const Qrcode = () => {
             <Flex
                 justifyContent={{base:'center'}}
                 alignItems={{base:'center'}}
-                pt={{base:'1.5em', md:'2em'}}
+                pt={{base:'1em'}}
                 flexDir={{base:'column', sm:'row'}}
             >
                 {
-                    ([1,2].map((item, index) => (
-                        <Box
+                    ([{
+                        id: 1,
+                        bank: 'Techcombank',
+                        name: 'LUONG MINH NGOC',
+                        number: '1903 6239 9140 19'
+                    },
+                    {
+                        id: 2,
+                        bank: 'Vietcombank',
+                        name: 'PHAN NGUYEN THANH VIET',
+                        number: '0421 000 535 750'
+                    }].map((item, index) => (
+                        <Flex
                             key={index}
-                            w={{base:'60%', sm:'10em', md:'12em'}}
-                            m={{base:'1em', sm:'1em 7%', md:'1em 4em'}}
+                            flexDir={{base:'column'}}
+                            justifyContent={{base:'center'}}
+                            alignItems={{base:'center'}}
+                            m={{base:'1em', sm:'1em 7%', md:'1em 4em', lg:'1em 5em', xl:'1em 5.5em'}}
+                            textAlign={{base:'center'}}
                         >
                             <Image
-                                src={`/images/qrcode${item}.jpg`}
-                                alt={`QR-Code-${item}`}
-                                borderRadius={{base:'1em'}}
+                                src={`/images/bank${item.id}.png`}
+                                alt={`${item.bank}`}
+                                w={{base:'40%', sm:'5em', md:'7em'}}
+                                h={{base:'auto', md:'4.5em'}}
+                                m={{base:'auto'}}
+                                p={{base:'0 0 0.75em'}}
+                                objectFit={{base:'contain'}}
+                            />
+
+                            <Image
+                                src={`/images/qrcode${item.id}.jpg`}
+                                alt={`QR-Code-${item.bank}`}
+                                w={{base:'60%', sm:'10em', md:'12em'}}
                                 m={{base:'auto'}}
                                 p={{base:'0'}}
                             />
-                        </Box>
+
+                            <Text
+                                fontWeight={{base:'500'}}
+                                fontSize={{base:'1.125em', md:'1.25em'}}
+                                pt={{base:'0.75em'}}
+                            >
+                                {item.name}
+                            </Text>
+                            <Text
+                                fontSize={{base:'1em', md:'1.125em'}}
+                            >
+                                {item.number}
+                            </Text>
+                        </Flex>
                     )
                     ))
                 }
