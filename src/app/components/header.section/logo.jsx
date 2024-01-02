3c8b50b26ab2b8aa61b5d1d80e7f3db1bf084a8a
nnguyet <nguyetlms@gmail.com>
2024-01-02 13:32:45 +0700

    change logo size smaller (desktop)

--- src/app/components/header.section/logo.jsx
+++ src/app/components/header.section/logo.jsx
@@ -3,7 +3,7 @@ import { Image, Flex } from '@chakra-ui/react'
 export const Logo = () => {
     return (
         <Flex
-            w={{base:'3.75em', md:'4em', lg:'5em'}}
+            w={{base:'3.75em', md:'4em', lg:'4.75em'}}
             mt={{base:'0.75em', md:'0'}}
             borderRadius={{base:'50%'}}
             border={{base:'1px dotted white'}}
@@ -13,7 +13,7 @@ export const Logo = () => {
             <Image
                 src='/images/logo.png'
                 alt='monogram'
-                w={{base:'70%', md:'78%', lg:'80%'}}
+                w={{base:'65%', md:'78%', lg:'80%'}}
                 filter={{base:'invert(100%)'}}
                 mb={{base:'-0.25em', lg:'-0.3em'}}
                 px={{md:'0.125em', lg:'0.25em'}}
