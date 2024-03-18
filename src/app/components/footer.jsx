60d4ef89d6b649e06aa260e517a446ec84e13364
nnguyet <nguyetlms@gmail.com>
2024-03-19 01:33:28 +0700

    add thank you footer

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -1,12 +1,45 @@
-import { Box, Text, Image } from '@chakra-ui/react'
+import { Text, Image, Flex, Link } from '@chakra-ui/react'
+import NextLink from 'next/link';
+// import for icon
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCopyright } from '@fortawesome/free-regular-svg-icons'
 
 export const Footer = () => {
+    const info = {
+        'Điều phối': 'DarlingnPlanners',
+        'Địa điểm tiệc VT': 'AromaCenter',
+        'Địa điểm tiệc SG': 'Pavillon Tân Sơn Nhất',
+        'M.U.A': 'Leps.Trinh Makeup',
+        'M.U.A tiệc VT': 'Hà Thị',
+        'Áo dài cưới': 'La Richesse Boutique',
+        'Suit CR': 'Umvest',
+        'Váy cuới': 'Kasew Bridal, Hestia Bridal',
+        'Hình prewedding': 'The Love Museum',
+        'Hình phóng sự cưới VT': 'Lệnh Đức - Tiệm ảnh dạo',
+        'Hình phóng sự cưới SG & gia tiên': 'Will Productions',
+        'Phim phóng sự cưới': '2Aproduction',
+        'MC': 'Nguyễn Trọng Hữu',
+        'Thiệp cưới': 'JAN Wedding Studio',
+        'Khung tranh rót cát': 'Dailyme.Mint',
+        'Hoa cưới': 'Fleurs des Moulie',
+        'Florist chụp prewedding VT': 'Mangala.flower',
+        'Mâm quả, decor nhà gái & tiệc VT': 'Dịch Vụ Cưới Hỏi Ngọc Trân',
+        'Decor nhà trai & tiệc SG': 'Concept & Decor by Trinhchaste',
+    }
+
     return (
-        <Box
+        <Flex
             bg={{base:'pink.default'}}
             p={{base:'1em 0.5em 0.125em', md:'1em 1em 0.25em', lg:'1.25em 1.5em 0.5em'}}
             className='font-nunito'
+            justifyContent={{base:'center'}}
+            alignItems={{base:'center'}}
+            flexDir={{base:'column'}}
         >
+            {/*
             <Text
                 textAlign={{base:'center'}}
                 fontStyle={{base:'italic'}}
@@ -35,12 +68,85 @@ export const Footer = () => {
                 <br />
                 Trân (Ori): 0938.301.168
             </Text>
-            {/* <Text
-                textAlign={{base:'center', md:'right'}}
-                fontSize={{base:'0.75em'}}
+            */}
+            <Flex
+                flexWrap='wrap'
+                justifyContent='center'
+                alignItems='center'
+                w={{base:'100%', md:'80%'}}
+                // w={{base:'calc(min(20.125rem, 90%))', sm:'25rem', md:'28rem'}}
+                // m={{base:'auto'}}
+                flexDirection={{base:'column', md:'row'}}
+                // display={{base:'inline-flex'}}
+                fontSize={{base:'0.875rem', md:'1rem', lg:'1.125rem'}}
             >
-                Develop by nnguyet
-            </Text> */}
-        </Box>
+                {
+                    Object.keys(info).map((key, index) => {
+                        return (
+                            <Flex
+                                key={index}
+                                flexWrap={{base:'wrap'}}
+                                justifyContent={{base:'center'}}
+                                alignItems={{base:'center'}}
+                                position={{base:'relative'}}
+                                mr={{base:'0', md:'1rem'}}
+                                sx={{'&:not(:last-child)::after':{
+                                    content:'" "',
+                                    position:'absolute',
+                                    right:'-0.5rem',
+                                    top:'15%',
+                                    width:'1px',
+                                    height:'70%',
+                                    backgroundColor:'#1A202C',
+                                    display:{base:'none', md:'block'}
+                                }}}
+                            >
+                                <Text
+                                    textAlign={{base:'center'}}
+                                    fontWeight={{base:'300'}}
+                                    // m={{base:'auto'}}
+
+                                >
+                                    {key} :
+                                </Text>
+                                <Text
+                                    textAlign={{base:'center'}}
+                                    fontWeight={{base:'600'}}
+                                    // m={{base:'auto'}}
+                                    ml={{base:'0.25rem'}}
+                                >
+                                    {info[key]}
+                                </Text>
+                            </Flex>
+                        )
+                    })
+                }
+            </Flex>
+
+            <Flex
+                fontStyle={{base:'italic'}}
+                m={{base:'1rem 0 0.25rem', md:'1.125rem 0 0.25rem'}}
+                fontSize={{base:'0.875rem', md:'1rem'}}
+            >
+                <Flex
+                    mt={{base:'0.1875rem'}}
+                    mr={{base:'0.125rem'}}
+                >
+                    <FontAwesomeIcon icon={faCopyright} />
+                </Flex>
+                2023 - 2024 Developed by
+                <Link
+                    as={NextLink}
+                    href='https://www.facebook.com/nguyet.29.21.3/'
+                    _hover={{textDecoration:'none', fontWeight:'bold'}}
+                    ml={{base:'0.25rem'}}
+                    target='_blank'
+                    rel='noreferrer noopener'
+                >
+                    nnguyet
+                </Link>
+            </Flex>
+
+        </Flex>
     )
 }
